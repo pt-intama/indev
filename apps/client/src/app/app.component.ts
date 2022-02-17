f3cd511867a04fd45866be1f5b1d6dc609fb307d
Ervan Arianto <ervanari88@gmail.com>
2022-02-18 01:15:51 +0700

    initial commit

---
+++ apps/client/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'indokku-platform-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'client';
+}
