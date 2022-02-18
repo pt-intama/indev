4596c465888cdeb5d5e387ba63c724ba6cf72bdc
Achmad Irianto Eka Putra <ryanbekhen.official@gmail.com>
2022-02-18 08:16:21 +0700

    write standard worker project

--- apps/worker/src/app/app.service.ts
+++ apps/worker/src/app/app.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to worker!' };
+  accumulate(data: number[]): number {
+    return (data || []).reduce((a, b) => a + b);
   }
 }
