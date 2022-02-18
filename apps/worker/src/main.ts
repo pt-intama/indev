2df91c722ded9962bf11671a8c1ab24acd272e63
Achmad Irianto Eka Putra <ryanbekhen.official@gmail.com>
2022-02-18 08:05:30 +0700

    add dependencies @nestjs/microservices for worker

--- apps/worker/src/main.ts
+++ apps/worker/src/main.ts
@@ -5,18 +5,23 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Transport, MicroserviceOptions } from '@nestjs/microservices';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
-  app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3333;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  const port = parseInt(process.env.PORT) || 3333;
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.TCP,
+      options: {
+        port: port,
+      },
+    }
   );
+  app.listen();
+  Logger.log(`🚀 Application is running on: tcp://localhost:${port}`);
 }
 
 bootstrap();
